5a4a645dee8a93c80e601ccadb2a6c84b9c702be
xhuyz <huylovengoc35@gmail.com>
2025-02-12 11:22:57 +0700

    add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './components/Header'
+import { Hero } from './components/Home/Hero'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
