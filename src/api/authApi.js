d1ed074cc0484799d160539189d944be1d70823b
XhuyZ <huylovengoc35@gmail.com>
2025-03-04 00:39:00 +0700

    add authContext for login process

---
+++ src/api/authApi.js
@@ -0,0 +1,12 @@
+import BASE_API_URL from "@/constants/api";
+import axios from "axios";
+
+export const accountLogin = async (payload) => {
+  try {
+    const response = await axios.post(`${BASE_API_URL}/login`, payload);
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Login failed");
+  }
+};
