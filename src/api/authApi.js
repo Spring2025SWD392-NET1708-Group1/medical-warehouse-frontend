c8c670d060b37262bb948da39cdcb3372c8cc864
XhuyZ <huylovengoc35@gmail.com>
2025-03-05 20:34:24 +0700

    Complete Staff add LotRequest

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const accountLogin = async (payload) => {
   try {
-    const response = await axios.post(`${BASE_API_URL}/login`, payload);
+    const response = await axios.post(`${BASE_API_URL}/auth/login`, payload);
     return response.data.data;
   } catch (error) {
     console.log(error);
